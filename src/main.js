3fe3fc4924f1bf284dbe02e97dcc9d3dcf5cfb08
“2432450223 <2432450223@qq.com>
2023-08-30 15:10:58 +0800

    2023.8.30

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // import ui from '../packages/index'
 import ui from 'test-9527-ui'
+import('test-9527-ui/style.css')
 const app = createApp(App)
 app.use(ui)
 app.mount('#app')
